66f0d5c1ceb097a57659ec724b49ecc1ad9c703f
DmitryStarov <314263@bk.ru>
2023-11-26 00:10:28 +0800

    refactoring/контент Cards  вынес в пропсы, сделал интерфейс для пропсов

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,22 +1,25 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import styles from './card.module.scss'
+import Link from 'next/link';
+import Image from 'next/image';
+import { ICardProps } from './Card.props';
+import styles from './card.module.scss';
 
-const Card = () => {
+const Card = (card: ICardProps) => {
   return (
     <li className={styles.card}>
       <Image
         className={styles.card__image}
-        src="/sports-teacher-in-gym-class 1.jpg"
+        src={card.imgUrl}
         alt="Обложка новости"
         width={360}
         height={210}
       />
-      <h2 className={styles.card__title}>Пример текста пример текста пример текста пример текста</h2>
-      <h3 className={styles.card__subtitle}>Пример текста</h3>
-      <p className={styles.card__text}>Пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример</p>
-      <Link className={styles.card__link} href='#'>Пример текста</Link>
+      <h2 className={styles.card__title}>{card.title}</h2>
+      <h3 className={styles.card__subtitle}>{card.subtitle}</h3>
+      <p className={styles.card__text}>{card.text}</p>
+      <Link className={styles.card__link} href={card.linkUrl}>
+        {card.linkText}
+      </Link>
     </li>
-  )
-}
-export { Card }
+  );
+};
+export { Card };
--- src/components/CardsList/CardsList.tsx
+++ src/components/CardsList/CardsList.tsx
@@ -1,16 +1,25 @@
-import styles from './CardsList.module.scss'
-import { Card } from '../Card/Card'
+import styles from './CardsList.module.scss';
+import { Card } from '../Card/Card';
+import { testCards } from '@/utils/constatns';
 const CardsList = () => {
   return (
     <section className={styles.cardslist}>
       <h2 className={styles.cardslist__title}>Пример текста</h2>
       <ul className={styles.cardslist__container}>
-        <Card />
-        <Card />
-        <Card />
+        {testCards.map((card) => (
+          <Card
+            key={card.id}
+            title={card.title}
+            subtitle={card.subtitle}
+            text={card.text}
+            imgUrl={card.imgUrl}
+            linkUrl={card.linkUrl}
+            linkText={card.linkText}
+          />
+        ))}
       </ul>
     </section>
-  )
-}
+  );
+};
 
-export { CardsList }
+export { CardsList };
