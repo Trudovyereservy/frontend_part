bef70cd466c6db7f115d4b902cb0e81e4ad6f7d6
DmitryStarov <314263@bk.ru>
2023-12-20 23:46:01 +0800

    refactoring/диструктуризация пропсов

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,21 +3,21 @@ import Image from 'next/image';
 import { ICardProps } from './Card.props';
 import styles from './card.module.scss';
 
-const Card = (card: ICardProps) => {
+const Card = ({ title, subtitle, text, imgUrl, linkUrl, linkText }: ICardProps) => {
   return (
     <li className={styles.card}>
       <Image
         className={styles.card__image}
-        src={card.imgUrl}
+        src={imgUrl}
         alt="Обложка новости"
         width={360}
         height={210}
       />
-      <h2 className={styles.card__title}>{card.title}</h2>
-      <h3 className={styles.card__subtitle}>{card.subtitle}</h3>
-      <p className={styles.card__text}>{card.text}</p>
-      <Link className={styles.card__link} href={card.linkUrl}>
-        {card.linkText}
+      <h2 className={styles.card__title}>{title}</h2>
+      <h3 className={styles.card__subtitle}>{subtitle}</h3>
+      <p className={styles.card__text}>{text}</p>
+      <Link className={styles.card__link} href={linkUrl}>
+        {linkText}
       </Link>
     </li>
   );
