9f868ec50d6a1ecaf075b1d015e51759a16360e4
Pavel Demin <pavlikdemin@yandex.ru>
2024-02-06 23:34:27 +0300

    Карточка тренера. Сделал раскрытие карточки.

--- src/components/CoachCards/Card/Card.tsx
+++ src/components/CoachCards/Card/Card.tsx
@@ -3,26 +3,37 @@ import Image from 'next/image';
 import { ICardCoachProps } from './Card.props';
 import styles from './Card.module.scss';
 import { coach } from '@/utils/constans';
-
+import { useState } from 'react';
 
 // const Card = ({ name, directions, achievements, imgUrl, linkUrl }: ICardCoachProps) => {
-const Card = () => {
+  const Card = () => {
+    const [openInfo, setOpenInfo] = useState(true);
+    
+    function openCard() {
+    setOpenInfo(!openInfo);
+  }
+
   return (
     <li className={styles.card}>
-      <div className={styles.card__image_wrapper}>
+      <div className={styles.card__photo_wrapper}>
         <Image
           className={styles.card__image}
-          src={coach.imgUrl}
-          alt="Обложка новости"
+          src={coach.photo}
+          alt="Фотография тренера"
           width={360}
           height={360}
         />
-        <span className={styles.card__image_title}>Пример</span>
+        <button className={styles.card__info_additional} onClick={openCard}></button>
       </div>
-      
-        <h2 className={styles.card__title}>{coach.name}</h2>
-        <p className={styles.card__text}>{coach.achievements}</p>
-      
+      {openInfo ? 
+      <div className={styles.card__info}>
+        <h3 className={styles.card__name}>{coach.surname}</h3>
+        <h3 className={styles.card__title}>{coach.name}</h3>
+        <h3 className={styles.card__title}>{coach.patronymic}</h3>
+        <p className={styles.card__directions}>{coach.directions}</p>
+      </div> : 
+      <p className={styles.card__achievements}>{coach.achievements}</p>
+      }
     </li>
   );
 };
