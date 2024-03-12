80dfed37d9481c41d512e5fded3f628557bee7c6
Pavel Demin <pavlikdemin@yandex.ru>
2024-03-13 00:48:44 +0300

    Настройка брейкпоинтов и стилей на разных разрешениях

--- src/components/FeedbacksSlider/SwiperTest/SwiperTest.jsx
+++ src/components/FeedbacksSlider/SwiperTest/SwiperTest.jsx
@@ -5,9 +5,16 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { sliderPosts } from '../../../utils/constans';
 import styles from './SwiperTest.module.scss';
 import '../../../styles/globals.scss';
-
-
 import './myswiper.scss';
+import {
+  BREAKPOINT_DESKTOP,
+  BREAKPOINT_LAPTOP,
+  BREAKPOINT_NETBOOK,
+  BREAKPOINT_TABLET,
+  BREAKPOINT_MOBILE,
+  BREAKPOINT_MOBILE_MIN,
+  // customBreakpoints
+} from '../../../utils/constResizeWindow';
 
 export default () => {
   return (
@@ -30,34 +37,62 @@ export default () => {
         // }}
         // speed={100}
         centeredSlides={true}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log('slide change')}
+        // onSwiper={(swiper) => console.log(swiper)}
+        // onSlideChange={() => console.log('slide change')}
         spaceBetween={35}
         grabCursor={true}
         pagination={{
           clickable: true,
         }}
         breakpoints={{
+            // customBreakpoints
           // when window width is >= 320px
+          // [BREAKPOINT_MOBILE_MIN]: {
           320: {
-            slidesPerView: 2,
-            spaceBetween: 30,
-          },
-          // when window width is >= 480px
-          394: {
+            // slidesPerView: 1,
             slidesPerView: 'auto',
-            spaceBetween: 35,
+            initialSlide: 0,
+            spaceBetween: 30,
+            loop:false,
           },
-          // when window width is >= 640px
-          // 640: {
-          //   slidesPerView: 4,
-          //   spaceBetween: 40
-          // }
+          // // when window width is >= 394px
+          // [BREAKPOINT_MOBILE]: {
+          395: {
+            // slidesPerView: 'auto',
+            slidesPerView: 2,
+            spaceBetween: 31,
+            loop:false,
+          },
+          // // when window width is >= 768px
+          // [BREAKPOINT_TABLET]: {
+          758: {
+            slidesPerView: 2,
+            spaceBetween: 5
+          },
+          // // // when window width is >= 1024px
+          // [BREAKPOINT_NETBOOK]: {
+          880: {
+            // slidesPerView: 4,
+            spaceBetween: 40
+          },
+          // // when window width is >= 1280px
+          // [BREAKPOINT_LAPTOP]: {
+          1024: {
+            // slidesPerView: 4,
+            spaceBetween: 35
+          },
+           // // when window width is >= 1440px
+          // [BREAKPOINT_DESKTOP]: {
+          1280: {
+            // slidesPerView: 4,
+            // spaceBetween: 35
+          }
         }}
+      
       >
         {sliderPosts.map((item, index) => (
           <SwiperSlide key={index} className="myslide">
-          {/* <SwiperSlide key={index} className={styles.myslide}> */}
+            {/* <SwiperSlide key={index} className={styles.myslide}> */}
             <p className={styles.feedbackCard__textPost}>{item.post}</p>
             <div className={styles.feedbackCard__about}>
               <p className={styles.feedbackCard__textName}>{item.name}</p>
--- src/utils/constResizeWindow.ts
+++ src/utils/constResizeWindow.ts
@@ -1,4 +1,24 @@
 export const BREAKPOINT_DESKTOP = 1440;
 export const BREAKPOINT_LAPTOP = 1024;
+export const BREAKPOINT_NETBOOK = 880;
+export const BREAKPOINT_TABLET = 768;
+export const BREAKPOINT_MOBILE = 394;
+export const BREAKPOINT_MOBILE_MIN = 330;
+
 export const CARD_COUNT_DESKTOP = 8;
 export const CARD_COUNT_LAPTOP = 9;
+
+// export const customBreakpoints = {
+//     350: {
+//       slidesPerView: 1,
+//       spaceBetween: 10
+//     },
+//     768: {
+//       slidesPerView: 3,
+//       spaceBetween: 20
+//     },
+//     1024: {
+//       slidesPerView: 4,
+//       spaceBetween: 30
+//     }
+//   };
