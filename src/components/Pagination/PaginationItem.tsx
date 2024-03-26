bb6d943b330f224a5e5701aac5de02b1391af788
Aleksey Tishenkov <55495+spacelessvoid@users.noreply.github.com>
2024-03-26 22:09:40 +0300

    добавил в компонент PaginationItem условие: не отображрать ссылку, если он выбран

--- src/components/Pagination/PaginationItem.tsx
+++ src/components/Pagination/PaginationItem.tsx
@@ -1,6 +1,6 @@
-import styles from "./Pagination.module.scss";
-import Link from "next/link";
-import classNames from "classnames";
+import styles from './Pagination.module.scss';
+import Link from 'next/link';
+import classNames from 'classnames';
 
 type PaginationItemProps = {
   href: string;
@@ -11,20 +11,24 @@ type PaginationItemProps = {
 export const PaginationItem = ({
   href,
   isSelected,
-  itemText = "#",
+  itemText = '#', //TODO Remove default value
 }: PaginationItemProps) => (
   <li
     className={classNames(styles.pagination__item, {
       [styles.pagination__item_type_selected]: isSelected,
     })}
   >
-    <Link
-      //TODO Update page # to use props
-      aria-label={`Go to page ${href}`}
-      className={styles.pagination__link}
-      href={href}
-    >
+    {isSelected ? (
       <span>{itemText}</span>
-    </Link>
+    ) : (
+      <Link
+        //TODO Update page # to use props
+        aria-label={`Go to page ${href}`}
+        className={styles.pagination__link}
+        href={href}
+      >
+        <span>{itemText}</span>
+      </Link>
+    )}
   </li>
 );
