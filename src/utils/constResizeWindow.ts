70bc9b6c1b20421ae336044b65f4352bb00f5132
Aleksey Tishenkov <55495+spacelessvoid@users.noreply.github.com>
2024-03-30 22:10:25 +0300

    fix: исправил constResizeWindow.ts
    добавил брейкпойнты в соответствии с макетом и _variables.scss; количество карточек для брейкпойнтов указал в соответствии с макетом версии 2

--- src/utils/constResizeWindow.ts
+++ src/utils/constResizeWindow.ts
@@ -1,4 +1,8 @@
 export const BREAKPOINT_DESKTOP = 1440;
 export const BREAKPOINT_LAPTOP = 1024;
-export const CARD_COUNT_DESKTOP = 8;
-export const CARD_COUNT_LAPTOP = 9;
+export const BREAKPOINT_NETBOOK = 880;
+export const BREAKPOINT_TABLET = 768;
+export const BREAKPOINT_MOBILE = 460;
+
+export const CARD_COUNT_DESKTOP = 9;
+export const CARD_COUNT_MOBILE = 8;
