e56f0c6ead08d04437c30a9c8302513b9f8c0494
Aleksey Tishenkov <55495+spacelessvoid@users.noreply.github.com>
2024-04-29 16:09:23 +0300

    обновил брейкпойнт в useCardCount

--- src/hooks/useCardCount.ts
+++ src/hooks/useCardCount.ts
@@ -1,20 +1,19 @@
 import { useMemo } from 'react';
 
 import {
-  BREAKPOINT_MOBILE,
+  BREAKPOINT_NETBOOK,
   CARD_COUNT_DESKTOP,
   CARD_COUNT_MOBILE,
 } from '../utils/constResizeWindow';
 
-const useCardCount = (width: number): number => {
-  return useMemo(() => {
+const useCardCount = (width: number): number =>
+  useMemo(() => {
     switch (true) {
-      case width <= BREAKPOINT_MOBILE:
+      case width <= BREAKPOINT_NETBOOK:
         return CARD_COUNT_MOBILE;
       default:
         return CARD_COUNT_DESKTOP;
     }
   }, [width]);
-};
 
 export { useCardCount };
