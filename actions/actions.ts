0365e80bcd39480e021c88584f66e955f8cee68d
GluKosAlex <glukosalex@gmail.com>
2024-05-04 12:12:57 +0700

    fix: add local host url

--- actions/actions.ts
+++ actions/actions.ts
@@ -3,7 +3,7 @@
 import { Coaches } from '@/api/generated/Coaches'
 
 export const getCoaches = async () => {
-  const coachesApi = new Coaches({ baseURL: process.env.API_URL });
+  const coachesApi = new Coaches({ baseURL: process.env.API_URL || 'http://localhost:3001' });
   const coaches = (await coachesApi.coachesList()).data
   return coaches
 }
