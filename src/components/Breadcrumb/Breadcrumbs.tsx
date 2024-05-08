49dff22f3e817f98c62f871cc29981eaa87b9254
Daniil Isaev <shandydenn@yandex.ru>
2024-05-09 06:08:47 +0700

    fix: поправил стили и функционал

--- src/components/Breadcrumb/Breadcrumbs.tsx
+++ src/components/Breadcrumb/Breadcrumbs.tsx
@@ -6,30 +6,56 @@ import styles from './Breadcrumbs.module.scss';
 
 import { IBreadcrumbs } from './Breadcrumbs.props';
 
-const RouteToLabelMap = {
+interface RouteToLabel {
+    blog: string;
+    coachs: string;
+    news: string;
+    contacts: string;
+}
+
+const RouteToLabelMap: RouteToLabel = {
     blog: "Блог",
     coachs: "Тренерский состав",
     news: "Новости",
     contacts: "Контакты"
 }
 
-const Breadcrumbs: FC<IBreadcrumbs> = () => {
-    const pathname = usePathname()
-    const segments = pathname.split("/");
-    segments.shift()
+const getLinks = (segments: Array<string>) => {
+    const links = segments.reduce((acc: Array<string>, cur, curIndex) => {
+        const last: string = curIndex > 0 ? acc[curIndex - 1] : "";
+        const newPath: string = last + "/" + cur;
+        acc.push(newPath);
+        return acc;
+    }, []);
+    return links;
+}
 
-    console.log(pathname);
-    console.log(segments);
+const Breadcrumbs: FC<IBreadcrumbs> = () => {
+    const pathname = usePathname();
+    const segments: Array<string> = pathname.split("/");
+    segments.shift()
+    const segmentsLinks = getLinks(segments);
+
+    const crumbs = segments.map((segment, index) => {
+        const crumb = {
+            href: segmentsLinks[index],
+            label: RouteToLabelMap[segment as keyof RouteToLabel],
+        };
+        return crumb;
+    })
 
     return (
-        <nav>
-            <ul>
-                <li>
+        <nav className={styles.breadcrumbs}>
+            <ul className={styles.breadcrumbs__list}>
+                <li className={styles.breadcrumbs__listItem}>
                     <Link href="/">Главная</Link>
                 </li>
                 {
-                    segments.map(segment =>
-                        <li>{RouteToLabelMap[segment]}</li>
+                    crumbs.map((segment) =>
+                        <li className={styles.breadcrumbs__listItem}>
+                            <p className={styles.breadcrumbs__separator}></p>
+                            <Link className={pathname === segment.href ? styles.breadcrumbs__activeLink : ""} href={segment.href}>{segment.label}</Link>
+                        </li>
                     )
                 }
             </ul>
