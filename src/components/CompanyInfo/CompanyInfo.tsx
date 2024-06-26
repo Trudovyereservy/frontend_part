71f98e7c83e82a2f9b1abc27f1cca1c5b1d75bde
Pavel Demin <pavlikdemin@ya.ru>
2024-06-27 00:43:41 +0300

    сделал 394 в черновом виде

--- src/components/CompanyInfo/CompanyInfo.tsx
+++ src/components/CompanyInfo/CompanyInfo.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+import useWindowSize from '@/hooks/useWindowSize';
+
 import { cardsCompanyInfo } from '../../utils/constants';
 import SwiperGalleryProvider from '../ProviderComponents/SwiperGalleryProvider';
 
@@ -6,14 +10,18 @@ import { CompanyInfoItem } from './CompanyInfoItem/CompanyInfoItem';
 import styles from './CompanyInfo.module.scss';
 
 const CompanyInfo = () => {
-
+  const widthWindow = useWindowSize();
   return (
     <section className={styles.companyinfo}>
-      <h2 className={styles.companyinfo__title}>Пример текста</h2>
-      <article className={styles.companyinfo__wrapper}>
+      {/* <div className={styles.companyinfo__titlewrapper}> */}
+      {/* </div> */}
+      {widthWindow <= 768 ? <article className={styles.companyinfo__wrapper}>
+        <h2 className={styles.companyinfo__title}>Пример текста</h2>
+        {/* <div className={styles.companyinfo */}
         <div className={styles.companyinfo__info}>
           <div className={styles.companyinfo__description}>Пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста</div>
           <div className={styles.companyinfo__items}>
+            <SwiperGalleryProvider />
             {cardsCompanyInfo.map((item) => (
               <CompanyInfoItem
                 key={item.id}
@@ -24,8 +32,29 @@ const CompanyInfo = () => {
             )}
           </div>
         </div>
-        <SwiperGalleryProvider />
       </article>
+        :
+        <article className={styles.companyinfo__wrapper}>
+          <h2 className={styles.companyinfo__title}>Пример текста</h2>
+          <div className={styles.companyinfo__infowrapper}>
+            <div className={styles.companyinfo__info}>
+              <div className={styles.companyinfo__description}>Пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста пример текста</div>
+              <div className={styles.companyinfo__items}>
+                {cardsCompanyInfo.map((item) => (
+                  <CompanyInfoItem
+                    key={item.id}
+                    textItem={item.textItem}
+                    subtitle={item.subtitle}
+                  />
+                )
+                )}
+              </div>
+            </div>
+            <SwiperGalleryProvider />
+          </div>
+        </article>
+      }
+
     </section>
   );
 };
