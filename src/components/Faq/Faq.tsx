398ae77ce37684d1cc7d66b96587d34863c72148
Pavel Demin <pavlikdemin@ya.ru>
2024-07-30 00:49:08 +0300

    пробный коммит №5 хаски и линт-стейжет

--- src/components/Faq/Faq.tsx
+++ src/components/Faq/Faq.tsx
@@ -4,14 +4,14 @@ import { testFaqItems } from '@/utils/constants';
 import styles from './Faq.module.scss';
 
 const Faq = () => (
-  <sectionf className={styles.faq}>
+  <section className={styles.faq}>
     <h2 className={styles.faq__title}>Пример текста</h2>
     <ul className={styles.faq__container}>
       {testFaqItems.map(({ title, text, id }) => (
         <FaqItem title={title} text={text} key={id} />
       ))}
     </ul>
-  </sectionf>
+  </section>
 );
 
 export { Faq };
