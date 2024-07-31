5029c66f3810f7094e831a06ee3e79a0afdda4fb
Pavel Demin <pavlikdemin@ya.ru>
2024-07-31 18:38:50 +0300

    test edit 1234

--- src/components/Faq/Faq.tsx
+++ src/components/Faq/Faq.tsx
@@ -1,8 +1,6 @@
 import { FaqItem } from '@/components/FaqItem/FaqItem';
 import { testFaqItems } from '@/utils/constants';
-
 import styles from './Faq.module.scss';
-
 const Faq = () => (
   <section className={styles.faq}>
     <h2 className={styles.faq__title}>Пример текста</h2>
