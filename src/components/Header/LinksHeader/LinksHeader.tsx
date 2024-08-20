8e8330fce77b9b661d43389216c5cd771b99dd7a
Pavel Demin <pavlikdemin@ya.ru>
2024-08-21 00:37:25 +0300

    тест1

--- src/components/Header/LinksHeader/LinksHeader.tsx
+++ src/components/Header/LinksHeader/LinksHeader.tsx
@@ -7,10 +7,8 @@ import styles from '../header.module.scss';
 
 const LinksHeader = (links: ILinksHeaderProps) => (
   <li className={styles.footer}>
-    <Link
-      className={classNames(styles.header__navitem, styles.header__navhidden)}
+    <Link className={classNames(styles.header__navitem, styles.header__navhidden)}>
       href={links.href}
-    >
       {links.title}
     </Link>
   </li>
